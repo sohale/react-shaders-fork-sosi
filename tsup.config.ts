5656af5356e888dfaff16c27b425408d552f9e98
Sohail Siadat <sohale@gmail.com>
2025-12-08 13:20:15 +0000

    minor: indentation/format

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,3 +1,7 @@
 import { defineConfig } from 'tsup'
 
-export default defineConfig([{ entry: ['core/index.tsx'], format: ['cjs', 'esm'], dts: true }])
+export default defineConfig([{
+  entry: ['core/index.tsx'],
+  format: ['cjs', 'esm'],
+  dts: true,
+}])
