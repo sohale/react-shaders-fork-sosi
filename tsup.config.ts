1f78b4aca04c2b14b2fb8416d41a3235aa25201a
Sohail Siadat <sohale@gmail.com>
2025-12-09 00:18:03 +0000

    modernise and use dist as compiler target dir

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,6 +4,9 @@ export default defineConfig([{
   entry: ['core/index.tsx'],
   format: ['cjs', 'esm'],
   dts: true,
+  outDir: 'dist',
+  sourcemap: true,
+  treeshake: true,
   external: ['react', 'react-dom'],
   esbuildOptions(options) {
     // THIS IS THE KEY FIX:
