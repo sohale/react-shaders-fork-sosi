e926f72ef3e52b21a74d73541293ad7d4d4e3d0f
Sohail Siadat <sohale@gmail.com>
2025-12-09 00:33:57 +0000

    improve other compiler options / build / ts-configurations

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig } from 'tsup'
 
-export default defineConfig([{
+export default defineConfig([
+  {
   entry: ['core/index.tsx'],
   format: ['cjs', 'esm'],
   dts: true,
   outDir: 'dist',
   sourcemap: true,
-  treeshake: true,
   external: ['react', 'react-dom'],
   esbuildOptions(options) {
-    // THIS IS THE KEY FIX:
     options.jsx = 'automatic'
   },
-}])
+  },
+])
